af7498800dae50073ca041e5c4d9b123910fb7b8
yesterday17 <2261302669@qq.com>
2014-10-04 15:24:36 +0800

    增加了关于快捷方式的内容
    可以自动添加快捷方式了~

--- src/org/yesterday17/graduation/MainActivity.java
+++ src/org/yesterday17/graduation/MainActivity.java
@@ -1,36 +1,118 @@
 package org.yesterday17.graduation;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.Intent.ShortcutIconResource;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		if(isAddShortCut()){
+			Toast toast = Toast.makeText(this, "已有快捷方式！", Toast.LENGTH_LONG);
+			toast.show();
+		}
+		else{
+			addShortCut();
+			Toast toast = Toast.makeText(this, "成功创建快捷方式！", Toast.LENGTH_LONG);
+			toast.show();
+		}
+		
+		// this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+		// this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		// WindowManager.LayoutParams.FLAG_FULLSCREEN);//去掉信息栏
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);		
+		return true;		
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	public boolean isAddShortCut() {
+
+		boolean isInstallShortcut = false;
+		final ContentResolver cr = this.getContentResolver();
+
+		int versionLevel = android.os.Build.VERSION.SDK_INT;
+		String AUTHORITY = "com.android.launcher2.settings";
+
+		// 2.2以上的系统的文件文件名字是不一样的
+		if (versionLevel >= 8) {
+			AUTHORITY = "com.android.launcher2.settings";
+		} else {
+			AUTHORITY = "com.android.launcher.settings";
+		}
+
+		final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
+				+ "/favorites?notify=true");
+		Cursor c = cr.query(CONTENT_URI,
+				new String[] { "title", "iconResource" }, "title=?",
+				new String[] { getString(R.string.app_name) }, null);
+
+		if (c != null && c.getCount() > 0) {
+			isInstallShortcut = true;
+		}
+		return isInstallShortcut;
+	}
+
+	public void addShortCut() {
+
+		Intent shortcut = new Intent(
+				"com.android.launcher.action.INSTALL_SHORTCUT");
+		// 设置属性
+		shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME,
+				getResources().getString(R.string.app_name));
+		ShortcutIconResource iconRes = Intent.ShortcutIconResource.fromContext(
+				this.getApplicationContext(), R.drawable.ic_launcher);
+		shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON, iconRes);
+
+		// 是否允许重复创建
+		shortcut.putExtra("duplicate", false);
+
+		// 设置桌面快捷方式的图标
+		Parcelable icon = Intent.ShortcutIconResource.fromContext(this,
+				R.drawable.ic_launcher);
+		shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
+
+		// 点击快捷方式的操作
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+		intent.addFlags(Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY);
+		intent.addCategory(Intent.CATEGORY_LAUNCHER);
+		//intent.setClass(AddShortCutActivity.this, AddShortCutActivity.class);
+
+		// 设置启动程序
+		shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent);
+
+		// 广播通知桌面去创建
+		this.sendBroadcast(shortcut);
+	}
 }
